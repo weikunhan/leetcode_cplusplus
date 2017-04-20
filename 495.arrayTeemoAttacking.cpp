1fd81766f9c9a0f2393319b56466976709bee4f2
Mark Han <weikunhan@g.ucla.edu>
2017-04-19 19:44:51 -0700

    Update 495.arrayTeemoAttacking.cpp

--- 495.arrayTeemoAttacking.cpp
+++ 495.arrayTeemoAttacking.cpp
@@ -5,13 +5,13 @@
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-		int res = 0;
-        int wakeTime = 0;
-        //if (timeSeries.empty()) return 0;
-		for (int i = 0; i < timeSeries.size(); i++) {
+       int res = 0;
+       int wakeTime = 0;
+       //if (timeSeries.empty()) return 0;
+	for (int i = 0; i < timeSeries.size(); i++) {
             if (timeSeries[i] < wakeTime) res = res + (timeSeries[i] + duration - wakeTime);
             else res = res + duration;
-			wakeTime = timeSeries[i] + duration;
+	    wakeTime = timeSeries[i] + duration;
         }
         return res;
     }
