46189352577165d1b604ba6a03fcfe9fa1fa3be1
Mark Han <weikunhan@g.ucla.edu>
2017-04-24 13:05:16 -0700

    Update and rename 442. arrayFindAllDuplicatesInAnArray.cpp to 442.findAllDuplicatesInAnArray.cpp

---
+++ 442.findAllDuplicatesInAnArray.cpp
@@ -0,0 +1,16 @@
+// 442. Find All Duplicates in an Array
+// C++
+// Tag: Array
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> res;
+	for (int i = 0; i < nums.size(); i++) {
+	    nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
+	    if (nums[abs(nums[i]) - 1] > 0) 
+                res.push_back(abs(nums[i]));
+	}
+	return res;
+    }
+};
