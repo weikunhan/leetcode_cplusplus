29268bf2202789ee1597479a803790ef66ce1fd0
Han::Mark <weikunhan@g.ucla.edu>
2017-04-30 12:47:53 -0700

    Update 496.nextGreaterElementI.cpp

--- 496.nextGreaterElementI.cpp
+++ 496.nextGreaterElementI.cpp
@@ -1,6 +1,15 @@
 // 496. Next Greater Element I
 // C++
 // Tag: Stack, Hash Table
+// Summary:
+// Suppose we have a decreasing sequence followed by a greater number.
+// For example [5, 4, 3, 2, 1, 6]. The greater number 6 
+// is the next greater element for all previous numbers in the sequence.
+// We use a stack to keep a decreasing sub-sequence, 
+// whenever we see a number x greater than stack.top(),
+// we pop all elements less than x and for all the popped ones, their next greater element is x
+// For example [9, 8, 7, 3, 2, 1, 6]. The stack will first contain [9, 8, 7, 3, 2, 1].
+// Then we see 6 which is greater than 1 so we pop 1 2 3 whose next greater element should be 6.
 
 class Solution {
 public:
