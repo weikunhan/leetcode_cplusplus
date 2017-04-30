82ee0db71ea2b70854f39296054b9baa762c9693
Han::Mark <weikunhan@g.ucla.edu>
2017-04-30 14:00:29 -0700

    Update 561.arrayPartitionI.cpp

--- 561.arrayPartitionI.cpp
+++ 561.arrayPartitionI.cpp
@@ -13,8 +13,8 @@ public:
     int arrayPairSum(vector<int>& nums) {
         int res = 0;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i = i + 2)
-            res = res + nums[i];
+        for (int i = 0; i < nums.size(); i += 2)
+            res += nums[i];
         return res;
     }
 };
