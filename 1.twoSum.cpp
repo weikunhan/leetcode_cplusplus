12651a1410426f8173fe0f036e6af08291562e25
Han::Mark <weikunhan@g.ucla.edu>
2017-06-29 11:49:45 -0700

    Update and rename 1 to 1.twoSum.cpp

---
+++ 1.twoSum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numbers;
+        vector<int> res;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int difference = target - nums[i];
+            if (numbers.find(difference) != numbers.end()) {
+                res.push_back(numbers[difference]);
+                res.push_back(i);
+            }
+            numbers[nums[i]] = i;
+        }
+        
+        return res;
+    }
+};
