8bc731cbf38940e12e5e95843deacb9abd315849
Han::Mark <weikunhan@g.ucla.edu>
2017-06-29 20:45:02 -0700

    Update 2.addTwoNumbers.cpp

--- 2.addTwoNumbers.cpp
+++ 2.addTwoNumbers.cpp
@@ -23,12 +23,12 @@ public:
         int carry = 0;
         
         while (l1 || l2 || carry) {
-            int sum = (l1 ? l1 -> val : 0) + (l2 ? l2 -> val : 0) + carry;
+            int sum = (l1 ? l1 -> val:0) + (l2 ? l2 -> val:0) + carry;
             carry = sum / 10;
             p -> next = new ListNode(sum % 10);
             p = p -> next;
-            l1 = l1 ? l1 -> next : l1;
-            l2 = l2 ? l2 -> next : l2;
+            l1 = l1 ? l1 -> next:l1;
+            l2 = l2 ? l2 -> next:l2;
         }
         
         return res -> next;
