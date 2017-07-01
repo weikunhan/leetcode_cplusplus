cb9d422c00bbb0d072c8da6b0864ccd350288c8f
Han::Mark <weikunhan@g.ucla.edu>
2017-07-01 09:43:42 -0700

    Create 7.reverseInteger.cpp

---
+++ 7.reverseInteger.cpp
@@ -0,0 +1,21 @@
+//==============================================================================
+// 7. Reverse Integer
+// C++
+// Tag: Math
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/reverse-integer/#/description
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        while(x) {
+            res = res*10 + x%10;
+            x /= 10;
+        }
+        
+        res = (res<INT_MIN || res>INT_MAX) ? 0 : res;
+        return res;
+    }
+};
