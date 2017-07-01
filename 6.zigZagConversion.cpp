bb9eab8f0e671190db06f8da9b2ff4a43da03aac
Han::Mark <weikunhan@g.ucla.edu>
2017-06-30 20:34:55 -0700

    Update 6.zigZagConversion.cpp

--- 6.zigZagConversion.cpp
+++ 6.zigZagConversion.cpp
@@ -18,11 +18,11 @@ public:
 
         for(int i = 0; i < numRows; i++) {
             for(int j = i; j < s.length(); j = j + cycle) {
-               res += s[j];
-               int k = j - i + cycle - i;
-               if(i > 0 && i < numRows - 1 && k < s.length()) {
-                   res += s[k];
-               }
+                res += s[j];
+                int k = j - i + cycle - i;
+                if(i > 0 && i < numRows - 1 && k < s.length()) {
+                    res += s[k];
+                }
             }
         }
         
