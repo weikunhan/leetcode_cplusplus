75a1b6df34cbbffb147fbaec637cea7bfe609757
Han::Mark <weikunhan@g.ucla.edu>
2017-06-30 20:33:23 -0700

    Create 6.zigZagConversion.cpp

---
+++ 6.zigZagConversion.cpp
@@ -0,0 +1,31 @@
+//==============================================================================
+// 6. ZigZag Conversion
+// C++
+// Tag: String
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/zigzag-conversion/#/description
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string res;
+        int cycle = 2 * numRows - 2;
+        
+        if (numRows <= 1) {
+            return s;
+        }
+
+        for(int i = 0; i < numRows; i++) {
+            for(int j = i; j < s.length(); j = j + cycle) {
+               res += s[j];
+               int k = j - i + cycle - i;
+               if(i > 0 && i < numRows - 1 && k < s.length()) {
+                   res += s[k];
+               }
+            }
+        }
+        
+        return res;
+    }
+};
