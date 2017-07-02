754892f59eaae8022ff3d520289bffab06509ad8
Han::Mark <weikunhan@g.ucla.edu>
2017-07-01 20:43:04 -0700

    Create 013_romanToInteger.cpp

---
+++ 013_romanToInteger.cpp
@@ -0,0 +1,32 @@
+//==============================================================================
+// 13. Roman to Integer
+// C++
+// Tag: Math
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/roman-to-integer/#/description
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> tables = {{'I', 1},
+                                           {'V', 5},
+                                           {'X', 10},
+                                           {'L', 50},
+                                           {'C', 100},
+                                           {'D', 500},
+                                           {'M', 1000}};
+        int res = 0;
+        
+        for(int i = 0; i < s.length() - 1; i++) { 
+            if(tables[s[i]] < tables[s[i + 1]]) {
+                res -= tables[s[i]];
+            } else {
+                res += tables[s[i]];
+            } 
+        }
+        res += tables[s.back()];
+        
+        return res;
+    }
+};
