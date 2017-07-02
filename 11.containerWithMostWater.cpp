b81407d5985e5c2847d750e977b7094d82bbdc2a
Han::Mark <weikunhan@g.ucla.edu>
2017-07-01 17:19:48 -0700

    Create 11.containerWithMostWater.cpp

---
+++ 11.containerWithMostWater.cpp
@@ -0,0 +1,29 @@
+//==============================================================================
+// 11. Container With Most Water
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/container-with-most-water/#/description
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int res = 0;
+        int i = 0;
+        int j = height.size() -  1;
+        
+        while (i < j) {
+            int newHeight = min(height[i], height[j]);
+            res = max(res, (j - i) * newHeight);
+            while (height[i] <= newHeight && i < j) {
+                i++;
+            }
+            while (height[j] <= newHeight && i < j) {
+                j--;
+            }
+        }
+        
+        return res;
+    }
+};
