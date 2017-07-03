e1518e63662fe292f07d8be63faa499a004192a2
Han::Mark <weikunhan@g.ucla.edu>
2017-07-02 22:52:24 -0700

    Update 012_integerToRoman.cpp

--- 012_integerToRoman.cpp
+++ 012_integerToRoman.cpp
@@ -9,11 +9,11 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        static const vector<string> M = {"", "M", "MM", "MMM"};
+        static const vector<string> C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        static const vector<string> X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        static const vector<string> I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         string res;
-        vector<string> M = {"", "M", "MM", "MMM"};
-        vector<string> C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        vector<string> X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        vector<string> I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         
         return res = M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
     }
