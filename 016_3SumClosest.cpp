21742e0eb681780a39f017db86f57da7b849c80d
Han::Mark <weikunhan@g.ucla.edu>
2017-07-03 12:20:58 -0700

    Update 016_3SumClosest.cpp

--- 016_3SumClosest.cpp
+++ 016_3SumClosest.cpp
@@ -18,11 +18,11 @@ public:
             res = nums[0] + nums[1] + nums[2];
         }
         
-        for (int i = 0; i < nums.size(); i++) {
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == target) {
                     return res = target;
                 }
@@ -30,9 +30,9 @@ public:
                     res = sum;
                 }
                 if (sum > target) {
-                    k--;
+                    right--;
                 } else {
-                    j++;
+                    left++;
                 }
             } 
         }
