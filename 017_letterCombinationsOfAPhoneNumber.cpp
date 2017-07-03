217138c92e7c424304e80d5729f3dd17ff54c9dc
Han::Mark <weikunhan@g.ucla.edu>
2017-07-02 22:44:36 -0700

    Create 017_letterCombinationsOfAPhoneNumber.cpp

---
+++ 017_letterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,41 @@
+//==============================================================================
+// 17. Letter Combinations of a Phone Number
+// C++
+// Tag: Backtracking
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/#/description
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        static const vector<string> letter = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> res;
+        res.push_back("");
+        
+        if (digits.empty()) {
+            return res = {};
+        }
+        
+        for (int i = 0 ; i < digits.size(); ++i) {
+//            int number = digits[i] - 48;
+            int number = digits[i] - '0';
+            string candidate = letter[number];
+            vector<string> tmp;
+            if (number < 0 || number > 9) {
+                break;
+            }
+            if (candidate.empty()) {
+                continue;
+            }
+            for (int j = 0; j < candidate.size(); j++) {
+                for (int k = 0; k < res.size(); k++) {
+                    tmp.push_back(res[k] + candidate[j]);
+                }
+            }
+            res.swap(tmp);
+        } 
+        
+        return res;
+    }
+};
