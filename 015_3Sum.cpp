ac8e22579ec4163aaebfff61b3baa8de2c4a0985
Han::Mark <weikunhan@g.ucla.edu>
2017-07-03 12:18:34 -0700

    Update 015_3Sum.cpp

--- 015_3Sum.cpp
+++ 015_3Sum.cpp
@@ -33,7 +33,7 @@ public:
                     while (left < right && nums[left] == nums[left + 1]) {          
                         left++;
                     }
-                    while (left > right && nums[right] == nums[right - 1]) {
+                    while (left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
                     left++; 
