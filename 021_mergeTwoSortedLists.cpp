a70335a24ea7e6fbd8fd4ac74bc0294130cdfbe0
Han::Mark <weikunhan@g.ucla.edu>
2017-07-05 21:43:20 -0700

    Update 021_mergeTwoSortedLists.cpp

--- 021_mergeTwoSortedLists.cpp
+++ 021_mergeTwoSortedLists.cpp
@@ -19,20 +19,20 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *res = new ListNode(NULL);
-        ListNode *p = res;
+        ListNode *node = res;
         
         while (l1 && l2) {
             if (l1 -> val > l2 -> val) {
-                p -> next = l2;
+                node -> next = l2;
                 l2 = l2 -> next;
             } else {
-                p -> next = l1;
+                node -> next = l1;
                 l1 = l1 -> next;
             }
-            p = p -> next;
+            node = node -> next;
         }
         
-        p -> next = l1 ? l1:l2;
+        node -> next = l1 ? l1:l2;
         
         return res -> next;
     }
