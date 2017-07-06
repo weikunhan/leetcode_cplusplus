a97d5d2dd4e388e34065bb0f1941306c3aa562c6
Han::Mark <weikunhan@g.ucla.edu>
2017-07-05 17:28:31 -0700

    Create 025_reverseNodesInk-Group.cpp

---
+++ 025_reverseNodesInk-Group.cpp
@@ -0,0 +1,41 @@
+//==============================================================================
+// 25. Reverse Nodes in k-Group
+// C++
+// Tag: Linked List
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/reverse-nodes-in-k-group/#/description
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* node = head;
+        
+        for (int i = 0; i < k; i++) {
+            if (node == NULL) {
+                return head; 
+            }
+            node = node -> next;
+        }
+
+        ListNode* res = helper(head, node);
+        head -> next = reverseKGroup(node, k);
+        
+        return res;
+    }
+    
+private: 
+    ListNode* helper(ListNode* head, ListNode* last) {
+        ListNode* prev = last;
+        ListNode* current = head;
+        
+        while (current != last) {
+            ListNode* tmp = current -> next;
+            current -> next = prev;
+            prev = current;
+            current = tmp;
+        }
+        
+        return prev;
+    }
+};
