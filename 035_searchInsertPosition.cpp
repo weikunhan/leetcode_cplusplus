11ab787f8f8678ff9d98004f1014663638aebffa
Han::Mark <weikunhan@g.ucla.edu>
2017-07-05 21:13:49 -0700

    Create 035_searchInsertPosition.cpp

---
+++ 035_searchInsertPosition.cpp
@@ -0,0 +1,30 @@
+//==============================================================================
+// 35. Search Insert Position
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/search-insert-position/#/description
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int res = 0;
+        int low = 0;
+        int high = nums.size() - 1;
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                return res = mid;
+            }
+        }
+
+        
+        return res = low;
+    }
+};
