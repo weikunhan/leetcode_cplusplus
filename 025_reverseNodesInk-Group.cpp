af861f028596b6a9997d0d32b6c8d125a271b50a
Han::Mark <weikunhan@g.ucla.edu>
2017-07-05 17:31:05 -0700

    Update 025_reverseNodesInk-Group.cpp

--- 025_reverseNodesInk-Group.cpp
+++ 025_reverseNodesInk-Group.cpp
@@ -6,6 +6,15 @@
 // Summary:
 // https://leetcode.com/problems/reverse-nodes-in-k-group/#/description
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
