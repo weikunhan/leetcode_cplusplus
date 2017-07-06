ae56a04aa501e2cbb345879160f9d256cd779fb4
Han::Mark <weikunhan@g.ucla.edu>
2017-07-05 17:39:49 -0700

    Create 027_removeElement.cpp

---
+++ 027_removeElement.cpp
@@ -0,0 +1,26 @@
+//==============================================================================
+// 27. Remove Element
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/remove-element/#/description
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int res = 0;
+        int left = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                left++;
+            } else {
+                nums[i - left] = nums[i];
+            }
+        }
+        
+        return res = nums.size() - left;
+
+    }
+};
