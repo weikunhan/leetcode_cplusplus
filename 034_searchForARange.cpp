4cd8b380cc6f59f5e44d2505ba868d5c217bc64c
Han::Mark <weikunhan@g.ucla.edu>
2017-07-06 13:37:55 -0700

    Update and rename 1.cpp to 034_searchForARange.cpp

---
+++ 034_searchForARange.cpp
@@ -0,0 +1,45 @@
+//==============================================================================
+// 34. Search for a Range
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/search-for-a-range/#/description
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res;
+        int low = 0;
+        int high = nums.size() - 1;
+        int left = -1;
+        int right = -1;
+        
+        if (low > high) {
+            return res = {left, right};
+        }
+        
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                left = mid;
+                right = mid;
+                break;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        while(left > 0  && nums[left-1] == target) {
+            left--;
+        }
+        
+        while(right < nums.size() && nums[right + 1] == target){
+            right++;
+        }
+        
+        return res = {left, right};
+    }
+};
