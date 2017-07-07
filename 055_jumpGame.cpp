5d7eb465db136d1ea5ce24b916b5f51663a49a5e
Han::Mark <weikunhan@g.ucla.edu>
2017-07-07 16:34:38 -0700

    Create 055_jumpGame.cpp

---
+++ 055_jumpGame.cpp
@@ -0,0 +1,24 @@
+//==============================================================================
+// 55. Jump Game
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/spiral-matrix/#/description
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        bool res = false;
+        int reachable = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > reachable) {
+                return res;
+            }
+            reachable = max(reachable, i + nums[i]);
+        }
+        
+        return res = true;
+    }
+};
