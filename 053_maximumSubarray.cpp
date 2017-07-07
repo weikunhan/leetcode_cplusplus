c24bed3314d9a426d7816b044b1c96ae4d369c89
Han::Mark <weikunhan@g.ucla.edu>
2017-07-07 13:32:05 -0700

    Update 053_maximumSubarray.cpp

--- 053_maximumSubarray.cpp
+++ 053_maximumSubarray.cpp
@@ -9,8 +9,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int res = nums[0];
+        int res = 0;
         int sum = 0;
+        
+        if (nums.empty()) {
+            return res;
+        } else {
+            res = nums[0];
+        }
         
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
