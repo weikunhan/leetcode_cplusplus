d887801239ce5fc709b7b793e99ff4de49715a0c
Han::Mark <weikunhan@g.ucla.edu>
2017-07-08 12:15:22 -0700

    Update 066_plusOne.cpp

--- 066_plusOne.cpp
+++ 066_plusOne.cpp
@@ -11,7 +11,7 @@ public:
     vector<int> plusOne(vector<int>& digits) {
       	vector<int> res;
         
-	for (int i = digits.size() - 1; i >= 0; --i) {
+	for (int i = digits.size() - 1; i >= 0; i--) {
 	    if (digits[i] == 9) {
 		digits[i] = 0;
 	    } else {
