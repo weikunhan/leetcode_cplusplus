f54cc81299619ea3c8bb84a118cd6169d22e1c34
Han::Mark <weikunhan@g.ucla.edu>
2017-07-13 16:11:48 -0700

    Update 067_addBinary.cpp

--- 067_addBinary.cpp
+++ 067_addBinary.cpp
@@ -11,8 +11,8 @@ public:
     string addBinary(string a, string b) {
         string res;
         int carry = 0; 
-        int i = a.length() - 1; 
-        int j = b.length() - 1;
+        int i = a.size() - 1; 
+        int j = b.size() - 1;
         
         while (i >= 0 || j >= 0 || carry == 1) {
             carry += (i >= 0) ? a[i--] - '0':0;
