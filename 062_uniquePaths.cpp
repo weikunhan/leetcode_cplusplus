cf7f199791d88cf6d39bbe671e30454bda902623
Han::Mark <weikunhan@g.ucla.edu>
2017-07-12 23:50:37 -0700

    Update 062_uniquePaths.cpp

--- 062_uniquePaths.cpp
+++ 062_uniquePaths.cpp
@@ -11,8 +11,8 @@ public:
     int uniquePaths(int m, int n) { 
         vector<vector<int>> res(m, vector<int>(n, 1));
         
-        for (int i = 1 ; i < m ; i++) {
-            for (int j = 1 ; j < n ; j++) {
+        for (int i = 1 ; i < m ; ++i) {
+            for (int j = 1 ; j < n ; ++j) {
                 res[i][j] = res[i - 1][j] + res[i][j - 1];
             }
         }
