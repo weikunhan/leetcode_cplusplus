a6f580d9919260ecca8c9bd2c23f8e951dda3c4d
Han::Mark <weikunhan@g.ucla.edu>
2017-07-13 16:09:14 -0700

    Update 058_lengthOfLastWord.cpp

--- 058_lengthOfLastWord.cpp
+++ 058_lengthOfLastWord.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int res = 0;
-        int i = s.length() - 1;
+        int i = s.size() - 1;
         
         while (i >= 0 && isspace(s[i])) {
             --i;
