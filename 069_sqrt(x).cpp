c578b75ecad7d08f57787db389ecca6983543b73
Han::Mark <weikunhan@g.ucla.edu>
2017-07-13 00:22:30 -0700

    Update 069_sqrt(x).cpp

--- 069_sqrt(x).cpp
+++ 069_sqrt(x).cpp
@@ -21,7 +21,7 @@ public:
             int mid = low + (high - low) / 2;
             if (mid > x / mid) {
                 high = mid - 1;
-            } else if (mid + 1 >  x / (mid + 1)){
+            } else if (mid + 1 >  x / (mid + 1)) {
                 return res = mid;
             } else {
                 low = mid +1;    
