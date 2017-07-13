555f4c4d725b2f19cb163102babbf26ca81831fd
Han::Mark <weikunhan@g.ucla.edu>
2017-07-12 21:05:12 -0700

    Update 055_jumpGame.cpp

--- 055_jumpGame.cpp
+++ 055_jumpGame.cpp
@@ -12,7 +12,7 @@ public:
         bool res = false;
         int reachable = 0;
         
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); ++i) {
             if (i > reachable) {
                 return res;
             }
