a79d52435f36d3d4b16729db5b34a807f751f03d
Han::Mark <weikunhan@g.ucla.edu>
2017-07-22 21:57:40 -0700

    Create 098_validateBinarySearchTree.cpp

---
+++ 098_validateBinarySearchTree.cpp
@@ -0,0 +1,39 @@
+//==============================================================================
+// 98. Validate Binary Search Tree
+// C++
+// Tag: Tree(DSF)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/validate-binary-search-tree/#/description
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        bool res = false;
+        
+        return res = helper(root, NULL, NULL);
+    }
+
+private:
+    bool helper(TreeNode* rootIn, TreeNode* minIn, TreeNode* maxIn) {
+        if (!rootIn) {
+            return true;
+        }
+        
+        if ((minIn && rootIn->val <= minIn->val) || (maxIn && rootIn->val >= maxIn->val)) {
+            return false;
+        }
+        
+        return helper(rootIn->left, minIn, rootIn) && helper(rootIn->right, rootIn, maxIn);
+    }
+};
