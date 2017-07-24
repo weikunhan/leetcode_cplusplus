dcfa75906d31b6bf6397925579b8411d6c030bb8
Han::Mark <weikunhan@g.ucla.edu>
2017-07-24 12:44:14 -0700

    Update 106_constructBinaryTreeFromInorderAndPostorderTraversal.cpp

--- 106_constructBinaryTreeFromInorderAndPostorderTraversal.cpp
+++ 106_constructBinaryTreeFromInorderAndPostorderTraversal.cpp
@@ -21,28 +21,28 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         TreeNode* res = NULL;
         
-        return res = helper(inorder, postorder, 0, inorder.size() - 1, 0, postorder.size() - 1);
+        return res = helper(0, inorder.size() - 1, 0, postorder.size() - 1, inorder, postorder);
     }
 
 private:
-    TreeNode* helper(vector<int>& inorderIn, vector<int>& postorderIn, int minIn1, int maxIn1, int minIn2, int maxIn2) {
+    TreeNode* helper(int minIn1, int maxIn1, int minIn2, int maxIn2, vector<int> &inorder, vector<int> &postorder) {
         int index = 0;
-        
+         
         if (minIn2 > maxIn2) {
             return NULL;
         }
         
-        TreeNode* root = new TreeNode(postorderIn[maxIn2]);
+        TreeNode* root = new TreeNode(postorder[maxIn2]);
     
         for (int i = minIn1; i <= maxIn1; ++i) {
-            if (inorderIn[i] == root->val) {
+            if (inorder[i] == root->val) {
                 index = i;
                 break;
             }
         }
         
-        root->left = helper(inorderIn, postorderIn, minIn1, index - 1, minIn2, minIn2 - minIn1 + index - 1);
-        root->right = helper(inorderIn, postorderIn, index + 1, maxIn1, maxIn2 - maxIn1 + index, maxIn2 - 1);
+        root->left = helper(minIn1, index - 1, minIn2, minIn2 - minIn1 + index - 1, inorder, postorder);
+        root->right = helper(index + 1, maxIn1, maxIn2 - maxIn1 + index, maxIn2 - 1, inorder, postorder);
         
         return root;
 
