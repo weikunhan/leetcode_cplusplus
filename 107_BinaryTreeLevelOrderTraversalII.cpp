c3650d30eef579f0b4e0e55387ea1f18a86b049a
Han::Mark <weikunhan@g.ucla.edu>
2017-07-24 19:37:27 -0700

    Create 107_BinaryTreeLevelOrderTraversalII.cpp

---
+++ 107_BinaryTreeLevelOrderTraversalII.cpp
@@ -0,0 +1,51 @@
+//==============================================================================
+// 107. Binary Tree Level Order Traversal II
+// C++
+// Tag: Tree(BFS)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/#/description
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        queue<TreeNode *> nodes;
+        
+        if (!root) {
+            return res;
+        }
+
+        nodes.push(root);
+        
+        while (!nodes.empty()) {
+            int level = nodes.size();
+            vector<int> tmp;
+            for (int i = 0; i < level; ++i) {
+                if (nodes.front()->left) {
+                    nodes.push(nodes.front()->left);
+                }
+                if (nodes.front()->right) {
+                    nodes.push(nodes.front()->right);
+                }
+                tmp.push_back(nodes.front()->val);
+                nodes.pop();
+            }
+            res.push_back(tmp);
+        }
+        
+        reverse(res.begin(), res.end());
+        
+        return res;
+    }
+};
