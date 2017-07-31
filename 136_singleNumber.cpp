7e76ecb1ec816b6bdb6951f902c92af39e11683e
Han::Mark <weikunhan@g.ucla.edu>
2017-07-30 23:42:43 -0700

    Create 136_singleNumber.cpp

---
+++ 136_singleNumber.cpp
@@ -0,0 +1,21 @@
+//==============================================================================
+// 136. Single Number
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/single-number/description/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        int firstState = 0; 
+        
+        for (int i = 0; i < nums.size(); ++i) {
+		        firstState = firstState ^ nums[i];
+        }
+        
+	      return result = firstState;
+    }
+};
