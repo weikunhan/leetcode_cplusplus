518b6721b06da179ec5621c6b498ee00b996038b
Han::Mark <weikunhan@g.ucla.edu>
2017-07-31 00:17:03 -0700

    Create 137_singleNumberII.cpp

---
+++ 137_singleNumberII.cpp
@@ -0,0 +1,23 @@
+//==============================================================================
+// 137. Single Number II
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/single-number-ii/description/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        int firstState = 0;
+        int secondState = 0;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            firstState = (firstState ^ nums[i]) & ~secondState;
+            secondState = (secondState ^ nums[i]) & ~firstState;
+        }
+        
+        return res = firstState;
+    }
+};
