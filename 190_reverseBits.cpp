4d1e87066600b438fa3a0c6bd193146af90ae1f5
Han::Mark <weikunhan@g.ucla.edu>
2017-08-02 11:58:58 -0700

    Create 190_reverseBits.cpp

---
+++ 190_reverseBits.cpp
@@ -0,0 +1,20 @@
+//==============================================================================
+// 190. Reverse Bits
+// C++
+// Tag: Bit Manipulation
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/reverse-bits/description/
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        
+        for (int i = 0; i < 32; ++i) {
+            res = (res << 1) + (n >> i & 1);
+        }
+        
+        return res;
+    }
+};
