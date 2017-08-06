d39a533dc7e220e280ccf9e592eb87a20560dc76
Han::Mark <weikunhan@g.ucla.edu>
2017-08-05 21:38:00 -0700

    Create 207_courseSchedule.cpp

---
+++ 207_courseSchedule.cpp
@@ -0,0 +1,46 @@
+//==============================================================================
+// 207. Course Schedule
+// C++
+// Tag: Graph(BFS, without DFS)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/course-schedule/description/
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<unordered_set<int>> graph(numCourses);
+        vector<int> degree(numCourses);
+        bool res = false;
+
+        for (int i = 0; i< prerequisites.size(); ++i) {
+            graph[prerequisites[i].second].insert(prerequisites[i].first);
+        }
+
+        for (int i = 0; i < graph.size(); ++i) {
+            for (auto n:graph[i]) {
+                ++degree[n];
+            }
+        }
+        
+        for (int i = 0; i < numCourses; ++i) {
+            int index = 0;
+            while (index < numCourses) {
+                if (!degree[index]) {
+                    break;
+                }
+                ++index;
+            }
+            if (index == numCourses) {
+                return res;
+            } else {
+                degree[index] = -1;
+            }
+            for (auto n:graph[index]) {
+                --degree[n];
+            }
+        }
+        
+        return res = true;
+    }
+};
