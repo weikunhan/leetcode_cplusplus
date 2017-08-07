1711153ff6921e33e54b1f28a044f523f3ec1b39
Han::Mark <weikunhan@g.ucla.edu>
2017-08-07 01:34:30 -0700

    Create 211_AddAndSearchWord-DataStructureDesign.cpp

---
+++ 211_AddAndSearchWord-DataStructureDesign.cpp
@@ -0,0 +1,91 @@
+//==============================================================================
+// 211. Add and Search Word - Data structure design
+// C++
+// Tag: Design, Backtracking(Trie)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/add-and-search-word-data-structure-design/description/
+
+class WordDictionary {
+public:
+    struct TrieNode {
+        bool isWord;
+        TrieNode *child[26];
+        TrieNode():isWord(false) {
+            memset(child, NULL, sizeof(TrieNode *) * 26);
+        }
+        ~TrieNode() {
+            for (auto n:child) {
+                if (n) { 
+                    delete n;
+                }
+            }
+        }
+    };
+    
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    ~WordDictionary() {
+        delete root;
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        TrieNode *tmp = root;
+        
+        for (int i = 0; i < word.size(); ++i) {
+            if (!tmp->child[word[i] - 'a']) {
+                tmp->child[word[i] - 'a'] = new TrieNode();
+            }
+            tmp = tmp->child[word[i] - 'a'];
+        }
+        
+        tmp->isWord = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        return helper(word, 0, root);
+    }
+    
+private:
+    TrieNode *root;
+    
+    bool helper(string wordIn, int stepIn, TrieNode *rootIn) {
+        if (stepIn == wordIn.size()) {
+            return false;
+        }
+        
+        if (wordIn[stepIn] != '.') {
+            if (!rootIn->child[wordIn[stepIn] - 'a']) {
+                return false;
+            }
+            if (stepIn == wordIn.size() - 1 && rootIn->child[wordIn[stepIn] - 'a']->isWord) {
+                return true;
+            }
+            return helper(wordIn, stepIn + 1, rootIn->child[wordIn[stepIn] - 'a']);
+        } else {
+            for (auto n:rootIn->child) {
+                if (n) {
+                    if (stepIn == wordIn.size() - 1 && n->isWord) {
+                        return true;
+                    }
+                    if (helper(wordIn, stepIn + 1, n)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * bool param_2 = obj.search(word);
+ */
