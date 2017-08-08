27409f60f609417cef121c59ca71e47499b9e4b7
Han::Mark <weikunhan@g.ucla.edu>
2017-08-07 22:09:27 -0700

    Create 217_containsDuplicate.cpp

---
+++ 217_containsDuplicate.cpp
@@ -0,0 +1,23 @@
+//==============================================================================
+// 217. Contains Duplicate
+// C++
+// Tag: Array(withour Hash Table)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        bool res = false;
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] == nums[i - 1]) {
+                return res = true;
+            }
+        }
+        
+        return res;
+    }
+};
