be79fcd04c3c88ea1cbfe7280583ee5a1a795c7d
Han::Mark <weikunhan@g.ucla.edu>
2017-08-08 22:39:12 -0700

    Create 218_theSkylineProblem.cpp

---
+++ 218_theSkylineProblem.cpp
@@ -0,0 +1,38 @@
+//==============================================================================
+// 218. The Skyline Problem
+// C++
+// Tag: Array(Heap)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/the-skyline-problem/description/
+
+class Solution {
+public:
+    vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<pair<int, int>> res;
+        int index = 0;
+        priority_queue<pair<int, int>> tables;
+        
+        while (index < buildings.size() || !tables.empty()) {
+            int currentX;
+            int currentHigh;
+            if (tables.empty() || (index < buildings.size() && buildings[index][0] <= tables.top().second)) {
+                currentX = buildings[index][0];
+                while (index < buildings.size() && buildings[index][0] == currentX) {
+                    tables.push(make_pair(buildings[index][2], buildings[index][1]));
+                    ++index;
+                }
+            } else {
+                currentX = tables.top().second;
+                while (!tables.empty() && tables.top().second <= currentX)
+                    tables.pop();
+            }
+            currentHigh = (tables.empty()) ? 0:tables.top().first;
+            if (res.empty() || res.back().second != currentHigh) {
+                res.push_back(make_pair(currentX, currentHigh));
+            }
+        }
+        
+        return res;
+    }
+};
