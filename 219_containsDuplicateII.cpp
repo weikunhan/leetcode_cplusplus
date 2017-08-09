d63427e96c01b81aa1513d199d63a8dd4751b483
Han::Mark <weikunhan@g.ucla.edu>
2017-08-09 11:24:03 -0700

    Create 219_containsDuplicateII.cpp

---
+++ 219_containsDuplicateII.cpp
@@ -0,0 +1,28 @@
+//==============================================================================
+// 219. Contains Duplicate II
+// C++
+// Tag: Array(Hash Table)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/contains-duplicate-ii/description/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> tables;
+        bool res = false;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (tables.find(nums[i]) == tables.end()) {
+                tables[nums[i]] = i;
+            } else { 
+                if (i - tables[nums[i]] <=k) {
+                    return res = true;
+                }
+                tables[nums[i]] = i;
+            }
+        }
+        
+        return res;
+    }
+};
