642b14e7f3bc634351add31b66fc274bb64735e2
Han::Mark <weikunhan@g.ucla.edu>
2017-08-11 14:46:40 -0700

    Create 123_bestTimeToBuyAndSellStockIII.cpp

---
+++ 123_bestTimeToBuyAndSellStockIII.cpp
@@ -0,0 +1,31 @@
+//==============================================================================
+// 123. Best Time to Buy and Sell Stock III
+// C++
+// Tag: Array(DP)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int number = 2;
+        vector<vector<int>> profit(number + 1, vector<int>(prices.size()));
+        int res = 0;
+        
+        if (prices.size() <= 1) {
+            return res;
+        }
+
+        for (int i = 1; i <= number; ++i) {
+            int tmpMax = profit[i - 1][0] - prices[0];
+            for (int j = 1; j < prices.size(); ++j) {
+                profit[i][j] = max(profit[i][j - 1], prices[j] + tmpMax);
+                tmpMax = max(tmpMax, profit[i - 1][j] - prices[j]);
+                res = max(profit[i][j], res);
+            }
+        }
+        
+        return res;
+    }
+};
