f77f7e2dc7ce4fe9c2e4de5d6bd9863fce697cc0
Han::Mark <weikunhan@g.ucla.edu>
2017-08-12 12:18:52 -0700

    Update 228_summaryRanges.cpp

--- 228_summaryRanges.cpp
+++ 228_summaryRanges.cpp
@@ -17,7 +17,7 @@ public:
             left = i;
             right = i;
             while (right + 1 < nums.size() && nums[right + 1] == nums[right] + 1) {
-                right++;
+                ++right;
             }
             if (right > left) {
                 res.push_back(to_string(nums[left]) + "->" + to_string(nums[right]));
