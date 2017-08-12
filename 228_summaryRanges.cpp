4b8c6ad04f9ff996d96e918be99f87462515d10d
Han::Mark <weikunhan@g.ucla.edu>
2017-08-11 17:19:37 -0700

    Create 228_summaryRanges.cpp

---
+++ 228_summaryRanges.cpp
@@ -0,0 +1,31 @@
+//==============================================================================
+// 228. Summary Ranges
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/summary-ranges/description/
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        int left = 0;
+        int right = 0;
+        
+        for (int i = 0; i < nums.size(); i = right + 1) {
+            left = i;
+            right = i;
+            while (right + 1 < nums.size() && nums[right + 1] == nums[right] + 1) {
+                right++;
+            }
+            if (right > left) {
+                res.push_back(to_string(nums[left]) + "->" + to_string(nums[right]));
+            } else {
+                res.push_back(to_string(nums[left]));
+            }
+        }
+        
+        return res;
+    }
+};
