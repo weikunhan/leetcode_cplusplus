57a588b00a84f48599eabbdb48d764792b8ef8e5
Han::Mark <weikunhan@g.ucla.edu>
2017-08-15 00:43:33 -0700

    Update 233. Number of Digit One.cpp

--- 233. Number of Digit One.cpp
+++ 233. Number of Digit One.cpp
@@ -1,32 +1,34 @@
- /**
- * Calculate occurance on every digit, from
- * least important digit to most important digit
- * number = 1432
- * One's digit: n/10=143 143*1+1
- * Ten's digit: n/100=14 14*10+10
- * Hun's digit: n/1000=1 1*100+100
- * Tho's digit: 1432%1000+1=433
- * Sum all occurance on digits together
- 
- For people who may not get the idea at the beginning (like me).
+//==============================================================================
+// 233. Number of Digit One
+// C++
+// Tag: Math
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/number-of-digit-one/description/
 
-One's digit: 143 * 1 + 1 => (11, 21, 31, ... 1431) + 1
-Ten's digit: 14* 10 + 10 => (110, 111, 112, ... 119, 210, 211, 212, .... 219, ....1419) + (10, 11, 12, ...19)
-Hundreds's digit: 1*100 + 100 => (1100, 1101, 1102, ...1199) + (100, 101,.... 199)
-Thousands's digit: 1432%100 + 1 => 1000, 1001, ... 1432
- */
-public static int countDigitOne(int k) {
-    int count = 0, factor = 1, n = k;
-    while(n>0){
-    	int m = n/10, r = n%10, amount;
-    	
-    	if(r == 0) amount = 0;
-    	else if(r > 1) amount = factor;
-    	else amount = k%factor+1;
- 
-    	count += m*factor + amount;
-    	factor *= 10;
-    	n = n/10;
+class Solution {
+public:
+    int countDigitOne(int n) {
+        int res = 0; 
+        int factor = 1; 
+        int tmp = n;
+        
+        while (tmp > 0) {
+    	    int digit = tmp / 10;
+            int indicator = tmp % 10;
+            int amount = 0;
+    	    if (indicator == 0) {
+                amount = 0;
+            } else if (indicator > 1) {
+                amount = factor;
+            } else {
+                amount = n % factor + 1;
+            }
+    	    res += digit *factor + amount;
+    	    factor *= 10;
+    	    tmp /=10;
+        }
+        
+        return res;
     }
-    return count;
-}
+};
