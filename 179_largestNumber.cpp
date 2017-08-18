b4ab968e5b73ad4bd7f33469d0a1d084e595b326
Han::Mark <weikunhan@g.ucla.edu>
2017-08-18 10:56:09 -0700

    Create 179_largestNumber.cpp

---
+++ 179_largestNumber.cpp
@@ -0,0 +1,36 @@
+//==============================================================================
+// 179. Largest Number
+// C++
+// Tag: Array
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/largest-number/description/
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> number;
+        string res;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            number.push_back(to_string(nums[i]));
+        }
+        
+        sort(begin(number), end(number), helper);
+
+        for (int i = 0; i < number.size(); ++i) {
+            res += number[i];
+        }
+        
+        while (res[0] == '0' && res.length() > 1) {
+            res.erase(0, 1);
+        }
+        
+        return  res;
+    }
+    
+private:
+    static bool helper(string &a, string &b) {
+        return a + b > b + a;
+    }
+};
