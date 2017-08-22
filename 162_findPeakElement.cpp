91be51cb9f0e63aceab1e68d9f70502bf66cec28
Han::Mark <weikunhan@g.ucla.edu>
2017-08-22 11:52:03 -0700

    Update 162_findPeakElement.cpp

--- 162_findPeakElement.cpp
+++ 162_findPeakElement.cpp
@@ -10,7 +10,7 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int low = 0;
-        int high = nums.size()-1;
+        int high = nums.size() - 1;
         int res = 0;
         
         while (low < high) {
