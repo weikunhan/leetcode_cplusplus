25147eb0d7024dd093d53b7d82d49233586fedc4
Han::Mark <weikunhan@g.ucla.edu>
2017-08-27 21:11:17 -0700

    Create 170_twoSumIII-DataStructureDesign.cpp

---
+++ 170_twoSumIII-DataStructureDesign.cpp
@@ -0,0 +1,40 @@
+//==============================================================================
+// 170. Two Sum III - Data structure design
+// C++
+// Tag: Design(Hash Table)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/two-sum-iii-data-structure-design/description/
+
+class TwoSum {
+public:
+    /** Initialize your data structure here. */
+    TwoSum() {}
+    
+    /** Add the number to an internal data structure.. */
+    void add(int number) {
+        ++tables[number];
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    bool find(int value) {
+        for (auto n:tables) {
+            int tmp = value - n.first;
+            if (tables.find(tmp) != tables.end() && (n.first != tmp || n.second > 1)) {
+                return true;
+            }
+        }
+            
+        return false;
+    }
+    
+private:
+    unordered_map<int, int> tables;
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * bool param_2 = obj.find(value);
+ */
