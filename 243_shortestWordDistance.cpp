61a8c1441857bd1d44bf577f5e16f80bb69e9c59
Han::Mark <weikunhan@g.ucla.edu>
2017-09-12 13:07:20 -0700

    Update 243_shortestWordDistance.cpp

--- 243_shortestWordDistance.cpp
+++ 243_shortestWordDistance.cpp
@@ -9,19 +9,19 @@
 class Solution {
 public:
     int shortestDistance(vector<string>& words, string word1, string word2) {
-        int position1 = -1;
-        int position2 = -1;
+        int index1 = -1;
+        int index2 = -1;
         int res = INT_MAX;
     
         for (int i = 0; i < words.size(); ++i) {
             if (words[i] == word1) {
-                position1 = i;
+                index1 = i;
             }
             if (words[i] == word2) {
-                position2 = i;
+                index2 = i;
             }
-            if (position1 != -1 && position2 != -1) {
-                res = min(res, abs(position1 - position2));
+            if (index1 != -1 && index2 != -1) {
+                res = min(res, abs(index1 - index2));
             }
         }
 
