c79d0bb0c83eb6157c815ab7b36420070a9fd6b3
Han::Mark <weikunhan@g.ucla.edu>
2017-09-18 16:05:01 -0700

    Create 258_addDigits.cpp

---
+++ 258_addDigits.cpp
@@ -0,0 +1,16 @@
+//==============================================================================
+// 258. Add Digits
+// C++
+// Tag: Math
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/add-digits/description/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        int res = 0;
+        
+        return res = 1 + (num - 1) % 9;
+    }
+};
