ff45ea66e84e3353b729a0d5d7f2d42815017b5c
Han::Mark <weikunhan@g.ucla.edu>
2017-09-19 11:19:26 -0700

    Create 259_3SumSmaller.cpp

---
+++ 259_3SumSmaller.cpp
@@ -0,0 +1,34 @@
+//==============================================================================
+// 259. 3Sum Smaller
+// C++
+// Tag: Array(2 Pointers)
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/3sum-smaller/description/
+
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        int res = 0;
+        sort(nums.begin(), nums.end());
+        
+        if (nums.size() < 1) {
+            return res;
+        }
+    
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] < target) {
+                    res += right - left;
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
