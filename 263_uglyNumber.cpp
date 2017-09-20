192877bf8f6107ccdc5404e73adab8840e82f01b
Han::Mark <weikunhan@g.ucla.edu>
2017-09-20 12:04:38 -0700

    Create 263_uglyNumber.cpp

---
+++ 263_uglyNumber.cpp
@@ -0,0 +1,26 @@
+//==============================================================================
+// 263. Ugly Number
+// C++
+// Tag: Math
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/ugly-number/description/
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        bool res = false;
+        
+        if (num <= 0) {
+            return res;
+        }
+        
+        for (int i = 2; i < 6; i++) {
+            while (num % i == 0) {
+                num /= i;
+            }
+        }
+        
+        return res = num == 1;
+    }
+};
