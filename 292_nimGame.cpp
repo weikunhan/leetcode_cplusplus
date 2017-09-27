149b900250fde88e54fa0f2f1a129f3d0ddf8ba0
Han::Mark <weikunhan@g.ucla.edu>
2017-09-26 19:08:10 -0700

    Create 292_nimGame.cpp

---
+++ 292_nimGame.cpp
@@ -0,0 +1,16 @@
+//==============================================================================
+// 292. Nim Game
+// C++
+// Tag: Math
+//==============================================================================
+// Summary:
+// https://leetcode.com/problems/nim-game/description/
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        bool res = false;
+        
+        return res = n % 4 != 0 ;
+    }
+};
