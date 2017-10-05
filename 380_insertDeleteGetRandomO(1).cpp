4bdfca161eecb837f86c7c05d0f892c0ccf976aa
Han::Mark <weikunhan@g.ucla.edu>
2017-10-04 23:59:33 -0700

    Update 380_insertDeleteGetRandomO(1).cpp

--- 380_insertDeleteGetRandomO(1).cpp
+++ 380_insertDeleteGetRandomO(1).cpp
@@ -37,7 +37,7 @@ public:
     }
     
     /** Get a random element from the set. */
-    /** Weikun Assume the vector is not empty */
+    /** Weikun: Assume the vector is not empty */
     int getRandom() {
         return number[rand() % number.size()];
     }
