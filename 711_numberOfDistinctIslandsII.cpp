6e3b579030458cd9191244af9a8bf97f0536baba
Han::Mark <weikunhan@g.ucla.edu>
2017-10-24 03:08:22 -0700

    Update 711_numberOfDistinctIslandsII.cpp

--- 711_numberOfDistinctIslandsII.cpp
+++ 711_numberOfDistinctIslandsII.cpp
@@ -8,7 +8,7 @@
 
 class Solution {
 public:
-    int numDistinctIslands(vector<vector<int>>& grid) {
+    int numDistinctIslands2(vector<vector<int>>& grid) {
         int number = 0;
         set<vector<pair<int, int>>> res;
         
@@ -28,7 +28,7 @@ private:
     unordered_map<int, vector<pair<int, int>>> tables;
     
     void helper1(int stepIn1, int stepIn2, int numberIn, vector<vector<int>> &grid) {
-        if (stepIn1 < 0 || stepIn2 < 0 || stepIn1 >= grid.size() || stepIn2 >= grid[0].size()) {
+        if (stepIn1 < 0 || stepIn2 < 0 || stepIn1 >= grid.size() || stepIn2 >= grid[stepIn1].size()) {
             return;
         }
         
@@ -36,29 +36,43 @@ private:
             return;
         }
         
-        grid[stepIn1][stepIn2] = 0;
         tables[numberIn].push_back({stepIn1, stepIn2});
+        grid[stepIn1][stepIn2] = 0;
         helper1(stepIn1 + 1, stepIn2, numberIn, grid);
         helper1(stepIn1, stepIn2 + 1, numberIn, grid);
         helper1(stepIn1 - 1, stepIn2, numberIn, grid);
         helper1(stepIn1, stepIn2 - 1, numberIn, grid);
     }
     
-    vector<pair<int,int>> helper2(vector<pair<int,int>> solutionIn) {
-        vector<pair<int,int>> tmp;
+    vector<pair<int, int>> helper2(vector<pair<int, int>> solutionIn) {
+        vector<vector<pair<int,int>>> tmp(8);
+
+        for (int i = 0 ; i < solutionIn.size(); ++i) {
+            int x = solutionIn[i].first;
+            int y = solutionIn[i].second;
+            tmp[0].push_back({x, y});
+            tmp[1].push_back({x, -y});
+            tmp[2].push_back({-x, y});
+            tmp[3].push_back({-x, -y});
+            tmp[4].push_back({y, -x});
+            tmp[5].push_back({-y, x});
+            tmp[6].push_back({-y, -x});
+            tmp[7].push_back({y, x});
+        }
         
-        for (int i = 0; i < solutionIn.size(); ++i) {
-            tmp.push_back({solutionIn[i].first, solutionIn[i].second});
+        for (int i = 0; i < tmp.size(); ++i) {
+            sort(tmp[i].begin(), tmp[i].end());
+        }
+        
+        for (int i = 0; i < tmp.size(); ++i) {
+            for (int j = 1; j < solutionIn.size(); ++j) {
+                tmp[i][j] = {tmp[i][j].first - tmp[i][0].first, tmp[i][j].second - tmp[i][0].second};
+            }
+            tmp[i][0] = {0, 0};
         }
         
         sort(tmp.begin(), tmp.end());
         
-        for (int i = 1; i < solutionIn.size(); ++i) {
-            tmp[i] = {tmp[i].first - tmp[0].first, tmp[i].second - tmp[0].second};
-        }
-        
-        tmp[0] = {0, 0};
-        
-        return tmp;
+        return tmp[0];
     }
 };
